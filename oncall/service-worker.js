20ccbe150f11d7885553d858538fe2358023b735
Everardo Madrid <154875903+evecima@users.noreply.github.com>
2026-08-29 01:40:32 -0400

    v0.5.15 cache period lifecycle module

--- oncall/service-worker.js
+++ oncall/service-worker.js
@@ -1,7 +1,7 @@
-const VERSION='0.5.14';
-const CACHE='oncall-maintenance-v0-5-14-ready-gate';
+const VERSION='0.5.15';
+const CACHE='oncall-maintenance-v0-5-15-period-lock';
 const V='?v='+VERSION;
-const CORE=["./","./index.html","./print.html"+V,"./styles-1.css"+V,"./styles-2.css"+V,"./styles-3.css"+V,"./styles-4.css"+V,"./styles-5.css"+V,"./styles-6.css"+V,"./styles-7.css"+V,"./vendor/html2canvas.min.js"+V,"./vendor/jspdf.umd.min.js"+V,"./ui-1.html"+V,"./ui-2.html"+V,"./ui-3.html"+V,"./ui-4.html"+V,"./seeds-1.js"+V,"./seeds-2.js"+V,"./seeds-3.js"+V,"./seeds-4.js"+V,"./app-01.js"+V,"./app-02.js"+V,"./app-03.js"+V,"./app-04.js"+V,"./app-05.js"+V,"./app-06.js"+V,"./app-07.js"+V,"./app-08.js"+V,"./app-09.js"+V,"./app-10.js"+V,"./app-11.js"+V,"../apartments.js"+V,"./manifest.webmanifest"+V,"./icons/icon.svg"+V];
+const CORE=["./","./index.html","./print.html"+V,"./styles-1.css"+V,"./styles-2.css"+V,"./styles-3.css"+V,"./styles-4.css"+V,"./styles-5.css"+V,"./styles-6.css"+V,"./styles-7.css"+V,"./vendor/html2canvas.min.js"+V,"./vendor/jspdf.umd.min.js"+V,"./ui-1.html"+V,"./ui-2.html"+V,"./ui-3.html"+V,"./ui-4.html"+V,"./seeds-1.js"+V,"./seeds-2.js"+V,"./seeds-3.js"+V,"./seeds-4.js"+V,"./app-01.js"+V,"./app-02.js"+V,"./app-03.js"+V,"./app-04.js"+V,"./app-05.js"+V,"./app-06.js"+V,"./app-07.js"+V,"./app-08.js"+V,"./app-09.js"+V,"./app-10.js"+V,"./app-11.js"+V,"./app-12.js"+V,"../apartments.js"+V,"./manifest.webmanifest"+V,"./icons/icon.svg"+V];
 const EXTERNAL=[
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js'
